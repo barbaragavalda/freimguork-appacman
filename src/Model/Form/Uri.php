33d57c3bacd7bf3e4a8a66b00774e7fabb0e0fac
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2018-05-14 10:10:07 +0200

    fixed: save uri correctly

--- src/Model/Form/Uri.php
+++ src/Model/Form/Uri.php
@@ -26,8 +26,9 @@ class Uri extends FormInput {
         $postValue = ($langID == null) ? $_POST['name'] : $_POST['name_'.$langID];
 
         $noTags = strip_tags($postValue);
-        $lowerCase = strtolower($noTags);
-        return urlencode( StringUtils::removeSpecialCharacters($lowerCase) );
+        $noSpecial = StringUtils::removeSpecialCharacters($noTags);
+        $lowerCase = strtolower($noSpecial);
+        return urlencode($lowerCase);
     }
 
     public function hasError($langID = null){
