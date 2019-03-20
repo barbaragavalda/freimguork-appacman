97e0eb18ab0361cc07f3d0384f24b406499f883c
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2019-03-20 09:27:30 +0000

    input address with possibility to add custom latitude and longitude

--- src/Model/Form/Address.php
+++ src/Model/Form/Address.php
@@ -34,10 +34,12 @@ class Address extends FormInput {
 
     protected function hasError($langID = null){
         $postValue = $this->getPostValue($langID);
+        $hasHiddenLocation = !empty($_POST['latitude-' . $this->fieldName]) && !empty($_POST['longitude-' . $this->fieldName]);
+        $hasLocation = !empty($_POST['latitude']) && !empty($_POST['longitude']);
         if( $postValue == null && $this->isRequired ){
             return gettext('Campo obligatorio.');
-        }else if( empty($_POST['latitude-' . $this->fieldName]) || empty($_POST['longitude-' . $this->fieldName]) ){
-            return gettext('AsegÃºrate de haber creado un marcador en el mapa con la direcciÃ³n correcta.');
+        }else if( !$hasHiddenLocation && !$hasLocation ){
+            return gettext('Asegúrate de haber creado un marcador en el mapa con la dirección correcta.');
         }
         return false;
     }
@@ -46,23 +48,25 @@ class Address extends FormInput {
     	$lat = $_POST['latitude-' . $this->fieldName];
     	$lng = $_POST['longitude-' . $this->fieldName];
     	
-    	if( $lat != $this->latitude || $lng != $this->longitude ){
-			$sql = '
-				UPDATE '.$this->table.'
-				SET latitude = :latitude, longitude = :longitude
-				WHERE id_'.$this->table.' = :id
-			';
-			$params = array(
-				'id'        => array('value' => $itemID,                                    'type' => \PDO::PARAM_INT),
-				'latitude'  => array('value' => $_POST['latitude-' . $this->fieldName],     'type' => \PDO::PARAM_STR),
-				'longitude' => array('value' => $_POST['longitude-' . $this->fieldName],    'type' => \PDO::PARAM_STR)
-			);
-			$this->mysql->query($sql, $params);
-			if( $this->mysql->getState() ){
-				return false;
+    	if( $lat && $lng ){
+			if( $lat != $this->latitude || $lng != $this->longitude ){
+				$sql = '
+					UPDATE '.$this->table.'
+					SET latitude = :latitude, longitude = :longitude
+					WHERE id_'.$this->table.' = :id
+				';
+				$params = array(
+					'id'        => array('value' => $itemID,	'type' => \PDO::PARAM_INT),
+					'latitude'  => array('value' => $lat,     	'type' => \PDO::PARAM_STR),
+					'longitude' => array('value' => $lng,    	'type' => \PDO::PARAM_STR)
+				);
+				$this->mysql->query($sql, $params);
+				if( $this->mysql->getState() ){
+					return false;
+				}
+				return true;
 			}
-			return true;
-    	}
+		}
     	return false;
     }
 
