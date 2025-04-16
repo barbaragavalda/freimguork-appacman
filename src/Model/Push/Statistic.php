e02e836d0d69d582a30a404424b94ffff20840ad
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2025-04-16 17:50:08 +0200

    push statistics

--- src/Model/Push/Statistic.php
+++ src/Model/Push/Statistic.php
@@ -5,35 +5,54 @@ namespace Appacman\Model\Push;
 use Core\Model\Model;
 use Core\Model\Push\Push;
 
-class Statistic extends Model {
+class Statistic extends Model
+{
 
     private $hasStatistics = false;
 
-    public function __construct($id){
+    public function __construct($id)
+    {
         parent::__construct();
 
-        $this->id = $id;
+        $this->id            = $id;
         $this->hasStatistics = $this->mysql->tableExists('appacman_push_statistic');
     }
 
-    public function update($devices){
-        if( $this->hasStatistics ){
-            $sql = '
+    public function update($devices)
+    {
+        if ($this->hasStatistics) {
+            $sql    = '
                 INSERT INTO appacman_push_statistic
                 SET id_appacman_push = :id,
                     devices = :devices
             ';
             $params = array(
-                'id'        => array('value' => $this->id,  'type' => \PDO::PARAM_INT),
-                'devices'   => array('value' => $devices,   'type' => \PDO::PARAM_INT),
+                'id'      => array('value' => $this->id, 'type' => \PDO::PARAM_INT),
+                'devices' => array('value' => $devices, 'type' => \PDO::PARAM_INT),
             );
-            if( $this->exists() ){
+            if ($this->exists()) {
                 $sql = '
                     UPDATE appacman_push_statistic
-                    SET devices = :devices
+                    SET devices = devices + :devices
                     WHERE id_appacman_push = :id
                 ';
             }
+
+            $this->mysql->query($sql, $params);
+        }
+    }
+
+    public function click()
+    {
+        if ($this->hasStatistics) {
+            $sql    = '
+                UPDATE appacman_push_statistic
+                SET clicks = clicks + 1
+                WHERE id_appacman_push = :id
+            ';
+            $params = array(
+                'id' => array('value' => $this->id, 'type' => \PDO::PARAM_INT),
+            );
             $this->mysql->query($sql, $params);
         }
     }
@@ -41,14 +60,15 @@ class Statistic extends Model {
     /**
      * @return bool|array     exists?
      */
-    private function exists(){
-        if( $this->hasStatistics ) {
-            $sql = '
-            SELECT *
-            FROM appacman_push_statistic
-            WHERE id_appacman_push = :id
-        ';
-            $params = array(
+    private function exists()
+    {
+        if ($this->hasStatistics) {
+            $sql       = '
+                SELECT *
+                FROM appacman_push_statistic
+                WHERE id_appacman_push = :id
+            ';
+            $params    = array(
                 'id' => array('value' => $this->id, 'type' => \PDO::PARAM_INT)
             );
             $statistic = $this->mysql->query($sql, $params);
@@ -60,14 +80,15 @@ class Statistic extends Model {
         return false;
     }
 
-    public function get(){
-        if( $this->hasStatistics ) {
-            $sql = '
-            SELECT devices
-            FROM appacman_push_statistic
-            WHERE id_appacman_push = :id
-        ';
-            $params = array(
+    public function get()
+    {
+        if ($this->hasStatistics) {
+            $sql       = '
+                SELECT devices, clicks
+                FROM appacman_push_statistic
+                WHERE id_appacman_push = :id
+            ';
+            $params    = array(
                 'id' => array('value' => $this->id, 'type' => \PDO::PARAM_INT)
             );
             $statistic = $this->mysql->query($sql, $params);
@@ -77,10 +98,11 @@ class Statistic extends Model {
             }
 
             return array(
-                'devices' => '0'
+                'devices' => '0',
+                'clicks' => '0'
             );
         }
         return array();
     }
-    
+
 }
